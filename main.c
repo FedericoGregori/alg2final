4c188b4ea7699e099722e72a1c316954b3805f9b
Federico Gregori <jfedericogregori@gmail.com>
2019-06-01 09:54:09 -0300

    Beginning List operations

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// -----------------------------------------
+//
+//    Estructuras
+//
+// -----------------------------------------
 
 typedef struct Amigo {
     char apellido[50];
     char nombre[50];
-    int num;
 } Amigo;
 
 typedef struct Nodo {
@@ -13,6 +19,34 @@ typedef struct Nodo {
 } Nodo;
 
 typedef struct Lista {
-    
-};
+    Nodo * head;
+} Lista;
+
+
+// -----------------------------------------
+//
+//    Operaciones con Nodos
+//
+// -----------------------------------------
+
+Nodo * crearNodo(Amigo * amigo) {
+    Nodo * nodo = (Nodo *) malloc(sizeof(Nodo));
+
+    strncpy(nodo->amigo.apellido, amigo->apellido, 50);
+	strncpy(nodo->amigo.nombre, amigo->nombre, 50);
+	
+    nodo->next = NULL;
+
+    return nodo;
+}
+
+void destruirNodo(Nodo * nodo) {
+    free(nodo);
+}
+
+// -----------------------------------------
+//
+//    Operaciones con Listas
+//
+// -----------------------------------------
 
